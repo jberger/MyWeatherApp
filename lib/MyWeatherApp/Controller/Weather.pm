20224edc3dfc07a3807c69252a40046825125eba
Joel Berger <joel.a.berger@gmail.com>
2017-12-06 07:39:23 -0600

    use a format that is easier on blog readers

--- lib/MyWeatherApp/Controller/Weather.pm
+++ lib/MyWeatherApp/Controller/Weather.pm
@@ -4,8 +4,12 @@ use Mojo::Base 'Mojolicious::Controller';
 sub recall {
   my $c = shift;
   my $search = $c->param('q');
-  return $c->render(status => 400, text => 'q parameter is required')
-    unless $search;
+
+  return $c->render(
+    status => 400,
+    text => 'q parameter is required',
+  ) unless $search;
+
   my $data = $c->weather->recall($search);
   $c->render(json => $data);
 }
